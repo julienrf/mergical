56e9f1241c3177b5bdee8f18899774ac8efe49ee
Julien Richard-Foy <julien@richard-foy.fr>
2013-01-04 22:23:20 +0100

    Another hashing function

--- app/models/Codec.scala
+++ app/models/Codec.scala
@@ -9,9 +9,9 @@ package models
  */
 object Codec {
 
-  def decode(value: String): Int = fromBase36(value) ^ Mask
+  def decode(value: String): Int = Integer.rotateRight(fromBase36(value), Shift)
 
-  def encode(n: Int): String = toBase36(n ^ Mask)
+  def encode(n: Int): String = toBase36(Integer.rotateLeft(n, Shift))
 
   def toBase36(n: Int): String = {
     val m = n / 36
@@ -23,7 +23,7 @@ object Codec {
   def fromBase36(v: String): Int =
     v.foldLeft(0) { (value, digit) => value * 36 + digits.indexOf(digit) }
 
-  val Mask = 0x12345678
+  val Shift = 10
 
   val digits = ('a' to 'z') ++ ('0' to '9')
 }
